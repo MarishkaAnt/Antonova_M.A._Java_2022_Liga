6686bd5a7d3a95c3a57aa3b3d11bc7b58917145f
Mariia Antonova <mariia_antonova@epam.com>
2022-06-22 23:45:25 +0300

    add tests

---
+++ homework-2/src/test/java/org/liga/CsvWriterTest.java
@@ -0,0 +1,17 @@
+package org.liga;
+
+import org.junit.jupiter.api.Test;
+import org.liga.filehandler.Writer;
+import org.liga.filehandler.impl.CsvWriter;
+
+import java.util.List;
+
+public class CsvWriterTest {
+    Writer writer = new CsvWriter();
+
+    @Test
+    void correctDataFormat_fileCreated(){
+        //given
+        //List<String> lines = List.of()
+    }
+}
