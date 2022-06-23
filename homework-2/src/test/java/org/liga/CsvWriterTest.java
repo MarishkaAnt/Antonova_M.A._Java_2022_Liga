bf0372dbcba2021cfe4283061b466b8e4b1f1e87
Mariia Antonova <mariia_antonova@epam.com>
2022-06-23 15:24:25 +0300

    implement test correctDataFormat_fileCreated()

--- homework-2/src/test/java/org/liga/CsvWriterTest.java
+++ homework-2/src/test/java/org/liga/CsvWriterTest.java
@@ -1,17 +1,23 @@
 package org.liga;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.liga.filehandler.Writer;
 import org.liga.filehandler.impl.CsvWriter;
 
+import java.nio.file.Path;
 import java.util.List;
 
 public class CsvWriterTest {
     Writer writer = new CsvWriter();
 
     @Test
-    void correctDataFormat_fileCreated(){
+    void correctDataFormat_fileCreated() {
         //given
-        //List<String> lines = List.of()
+        List<String> lines = List.of("1, тест, тест");
+        Path path = Path.of("src/test/resources/EmptyCsvForWriting.csv");
+
+        //then
+        Assertions.assertDoesNotThrow(() -> writer.write(lines, path));
     }
 }
