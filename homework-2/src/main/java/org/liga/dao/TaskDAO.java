a5221be6d6acb29af0db4e675e7ff5d22c8daed1
Mariia Antonova <mariia_antonova@epam.com>
2022-06-26 01:19:41 +0300

    add two methods

--- homework-2/src/main/java/org/liga/dao/TaskDAO.java
+++ homework-2/src/main/java/org/liga/dao/TaskDAO.java
@@ -1,4 +1,11 @@
 package org.liga.dao;
 
+import org.liga.model.Task;
+
+import java.util.List;
+
 public interface TaskDAO {
+
+    List<Task> findAllByStatus(String status);
+    List<Task> findAll();
 }
