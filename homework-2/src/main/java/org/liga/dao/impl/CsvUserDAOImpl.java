338d861fffee55e5d1f807de33f315305c9d7b2b
Mariia Antonova <mariia_antonova@epam.com>
2022-06-26 14:30:20 +0300

    add and implement all required methods

--- homework-2/src/main/java/org/liga/dao/impl/CsvUserDAOImpl.java
+++ homework-2/src/main/java/org/liga/dao/impl/CsvUserDAOImpl.java
@@ -15,7 +15,8 @@ import java.util.stream.Collectors;
 import static java.nio.file.StandardOpenOption.*;
 
 public class CsvUserDAOImpl implements UserDAO {
-    private static final String FILE_READING_ERROR = "Извините, что-то не так с файлами для хранения данных";
+    private static final String FILE_READING_ERROR = "Извините, что-то не так с файлами для хранения данных :";
+    public static final String USER_NOT_FOUND = "Пользователя с таким id не существует";
     private final Path path;
     private List<String> lines = new ArrayList<>();
     private List<User> users = new ArrayList<>();
@@ -25,19 +26,23 @@ public class CsvUserDAOImpl implements UserDAO {
         try {
             initialiseUsers();
         } catch (IOException e) {
-            System.out.println(FILE_READING_ERROR);
-            e.printStackTrace();
+            System.out.println(FILE_READING_ERROR + e.getMessage());
         }
     }
 
     private void initialiseUsers() throws IOException {
         if (!Files.isReadable(path)) {
-            throw new IOException("Couldn't read the file: " + path);
+            throw new IOException("Невозможно открыть файл: " + path);
         }
         lines.addAll(Files.readAllLines(path));
-        users.addAll(lines.stream()
-                .map(UserMapper::stringToUser)
-                .collect(Collectors.toList()));
+        if (lines.size() > 0) {
+            users.addAll(lines.stream()
+                    .map(UserMapper::stringToUser)
+                    .collect(Collectors.toList()));
+        } else {
+            throw new IOException("В файле нет данных " + path + "\n" +
+                    "Пожалуйста, добавьте минимум одного пользователя");
+        }
     }
 
     @Override
@@ -47,8 +52,9 @@ public class CsvUserDAOImpl implements UserDAO {
         }
         String line = UserMapper.userToString(user);
         lines.add(line);
+        users.add(user);
         try {
-            Files.write(path, lines, CREATE, APPEND);
+            Files.write(path, List.of(line), CREATE, APPEND);
         } catch (IOException e) {
             return false;
         }
@@ -65,11 +71,28 @@ public class CsvUserDAOImpl implements UserDAO {
         return users.stream()
                 .filter(u -> u.getId().equals(id))
                 .findAny().orElseThrow(() ->
-                        new WrongCommandParameters("Пользователя с таким id не существует"));
+                        new WrongCommandParameters(USER_NOT_FOUND));
     }
 
     @Override
-    public void deleteAll(){
+    public void deleteAll() {
+        lines.clear();
+        users.clear();
+        try {
+            Files.write(path, new ArrayList<>(), TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("It's impossible to delete users from file: " + path + " because of: ", e);
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        User founded = users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findAny().orElseThrow(() ->
+                        new WrongCommandParameters(USER_NOT_FOUND));
+        users.removeAll(List.of(founded));
+        lines.removeAll(List.of(UserMapper.userToString(founded)));
         try {
             Files.write(path, lines, TRUNCATE_EXISTING);
         } catch (IOException e) {
@@ -77,11 +100,33 @@ public class CsvUserDAOImpl implements UserDAO {
         }
     }
 
+    @Override
+    public void update(User user) {
+        List<User> updated = users.stream()
+                .map(u -> {
+                    if (u.getId().equals(user.getId())) {
+                        lines.remove(UserMapper.userToString(u));
+                        lines.add(UserMapper.userToString(user));
+                        u.setFirstName(user.getFirstName());
+                        u.setLastName(user.getLastName());
+                    }
+                    return u;
+                })
+                .collect(Collectors.toList());
+        users.clear();
+        users.addAll(updated);
+        try {
+            Files.write(path, lines, TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("It's impossible to delete users from file: " + path + " because of: ", e);
+        }
+
+    }
+
     private Boolean validateUser(User user) {
         return user.getId() != null &&
                 user.getFirstName() != null &&
                 user.getLastName() != null;
     }
-
 }
 
--- homework-2/src/test/java/org/liga/CsvUserDAOImplTest.java
+++ homework-2/src/test/java/org/liga/CsvUserDAOImplTest.java
@@ -1,63 +1,140 @@
 package org.liga;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.liga.dao.UserDAO;
 import org.liga.dao.impl.CsvUserDAOImpl;
+import org.liga.mapper.UserMapper;
 import org.liga.model.User;
 
 import java.io.IOException;
-import java.nio.file.NoSuchFileException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CsvUserDAOImplTest {
-    private static UserDAO UserDAO;
-    private static final String CORRECT_PATH = "src/test/resources/EmptyCsvForWriting.csv";
+    private static final String CORRECT_PATH = "src/test/resources/CorrectTestSample.csv";
     private static final String INCORRECT_PATH = "src/test/resources/notExists.csv";
+    private static final String EMPTY_FILE_PATH = "src/test/resources/EmptyTestSample.csv";
     private static final User EMPTY_USER = User.builder().build();
+    private static final List<String> initialLines = List.of("1, ПервыйТекст, ВторойТекст",
+            "2, Текст с пробелом, ТЕКСТ С ЗАГЛАВНЫМИ БУКВАМИ");
     private static final User CORRECT_USER = User.builder()
-                                                .id(4)
-                                                .firstName("name")
-                                                .lastName("name")
-                                                .build();
+            .id(4)
+            .firstName("name")
+            .lastName("name")
+            .build();
+    private static final User NEW_CORRECT_USER = User.builder()
+            .id(4)
+            .firstName("new name")
+            .lastName("new name")
+            .build();
 
-    @BeforeAll
-    static void init() {
-        Path path = Path.of(CORRECT_PATH);
-        try {
-            UserDAO = CsvUserDAOImpl.getInstance(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @AfterAll
-    static void clean() {
-        try {
-            UserDAO.deleteAll();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @AfterEach
+    public void clean() throws IOException {
+        Files.write(Path.of(CORRECT_PATH), initialLines);
     }
 
     @Test
     void addCorrectUser_nothingThrows() {
-
-        Assertions.assertDoesNotThrow(() -> UserDAO.add(CORRECT_USER));
+        //given
+        UserDAO userDAO = new CsvUserDAOImpl(Path.of(CORRECT_PATH));
+        //then
+        assertDoesNotThrow(() -> userDAO.create(CORRECT_USER));
     }
 
     @Test
-    void addCorrectUser_returnTrue() throws IOException {
-        Assertions.assertTrue(UserDAO.add(CORRECT_USER));
+    void addCorrectUser_returnTrue() {
+        //given
+        UserDAO userDAO = new CsvUserDAOImpl(Path.of(CORRECT_PATH));
+        //then
+        assertTrue(userDAO.create(CORRECT_USER));
     }
 
     @Test
-    void fileNotExist_throwIOException(){
+    void addCorrectUser_userAdded() throws IOException {
+        //given
+        Path path = Path.of(CORRECT_PATH);
+        UserDAO userDAO = new CsvUserDAOImpl(path);
+        Files.write(path, List.of(UserMapper.userToString(CORRECT_USER)), APPEND);
+        List<String> expected = Files.readAllLines(path);
+        Files.write(path, initialLines);
+        //when
+        userDAO.create(CORRECT_USER);
+        List<String> actual = Files.readAllLines(path);
+        //then
+        assertEquals(expected, actual);
+    }
 
-        Assertions.assertThrows(NoSuchFileException.class,
-                () -> CsvUserDAOImpl.getInstance(Path.of(INCORRECT_PATH)));
+    @Test
+    void findAll_actualEqualsExpected() throws IOException {
+        //given
+        Path localPath = Path.of("src/test/resources/CorrectTestSample.csv");
+        UserDAO localUserDAO = new CsvUserDAOImpl(localPath);
+        List<String> lines = Files.readAllLines(localPath);
+        List<User> expected = lines.stream()
+                .map(UserMapper::stringToUser)
+                .collect(Collectors.toList());
+        //when
+        List<User> actual = localUserDAO.findAll();
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addEmptyUser_returnFalse() {
+        //given
+        UserDAO userDAO = new CsvUserDAOImpl(Path.of(CORRECT_PATH));
+        //then
+        assertFalse(userDAO.create(EMPTY_USER));
+    }
+
+    @Test
+    void fileNotExist_notingThrow() {
+        assertDoesNotThrow(() -> new CsvUserDAOImpl(Path.of(INCORRECT_PATH)));
+    }
+
+    @Test
+    void emptyFileInitialisation_notingThrow() {
+        assertDoesNotThrow(() -> new CsvUserDAOImpl(Path.of(EMPTY_FILE_PATH)));
+    }
+
+    @Test
+    void addCorrectUserToEmptyFile_returnTrue() {
+        //given
+        CsvUserDAOImpl localUserDao = new CsvUserDAOImpl(Path.of(EMPTY_FILE_PATH));
+        //when
+        Boolean isCreated = localUserDao.create(CORRECT_USER);
+        localUserDao.deleteAll();
+        //then
+        assertTrue(isCreated);
+    }
+
+    @Test
+    void deleteById_correctId_nothingThrow() {
+        //given
+        Path path = Path.of(CORRECT_PATH);
+        UserDAO userDAO = new CsvUserDAOImpl(path);
+        //then
+        assertDoesNotThrow(() -> userDAO.deleteById(2));
+    }
+
+    @Test
+    void update_correctNewUser_userUpdated() {
+        //given
+        Path path = Path.of(CORRECT_PATH);
+        UserDAO userDAO = new CsvUserDAOImpl(path);
+        userDAO.deleteAll();
+        userDAO.create(CORRECT_USER);
+        //when
+        userDAO.update(NEW_CORRECT_USER);
+        List<User> actual = userDAO.findAll();
+        //then
+        assertEquals(List.of(NEW_CORRECT_USER), actual);
     }
 
 }
