b787a8106cad1d1053e41734121e1efb3cb8314f
Mariia Antonova <mariia_antonova@epam.com>
2022-06-26 15:33:05 +0300

    add all required methods

--- homework-2/src/main/java/org/liga/dao/TaskDAO.java
+++ homework-2/src/main/java/org/liga/dao/TaskDAO.java
@@ -3,9 +3,23 @@ package org.liga.dao;
 import org.liga.model.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskDAO {
 
     List<Task> findAllByStatus(String status);
+
+    void changeStatus(String status);
+
     List<Task> findAll();
+
+    Optional<Task> findById(Integer id);
+
+    Boolean create(Task task);
+
+    void deleteAll();
+
+    void deleteById(Integer id);
+
+    void update(Task task);
 }
