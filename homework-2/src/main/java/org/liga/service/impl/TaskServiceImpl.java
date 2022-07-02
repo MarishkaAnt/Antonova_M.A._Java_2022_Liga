f438b21d9694e9e8b3a56f49128b49c6b95897e1
Mariia Antonova <mariia_antonova@epam.com>
2022-07-02 22:22:37 +0300

    implement service

---
+++ homework-2/src/main/java/org/liga/service/impl/TaskServiceImpl.java
@@ -0,0 +1,66 @@
+package org.liga.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.liga.dao.TaskRepository;
+import org.liga.enums.Status;
+import org.liga.mapper.TaskMapper;
+import org.liga.model.Task;
+import org.liga.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Override
+    public List<Task> findAllByStatus(Status status) {
+        return taskRepository.findByStatus(status);
+    }
+
+    @Override
+    public void changeStatus(Integer id, String status) {
+        Task task = taskRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        task.setStatus(Status.valueOf(status));
+        taskRepository.save(task);
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Optional<Task> findById(Integer id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Task> create(String parametersLine) {
+        Task task = TaskMapper.stringToTask(parametersLine);
+        return Optional.of(taskRepository.save(task));
+    }
+
+    @Override
+    public void deleteAll() {
+        taskRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        taskRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Task> update(Integer id, String parametersLine) {
+        taskRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        Task newTask = TaskMapper.stringToTask(parametersLine);
+        newTask.setId(id);
+        return Optional.of(taskRepository.save(newTask));
+    }
+}
---
+++ homework-2/src/main/java/org/liga/service/impl/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package org.liga.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.liga.dao.UserRepository;
+import org.liga.mapper.UserMapper;
+import org.liga.model.User;
+import org.liga.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Optional<User> create(String parametersLine) {
+        User user = UserMapper.stringToUser(parametersLine);
+        return Optional.of(userRepository.save(user));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        userRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<User> update(Integer id, String parametersLine) {
+        userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        User newUser = UserMapper.stringToUser(parametersLine);
+        newUser.setId(id);
+        return Optional.of(userRepository.save(newUser));
+    }
+}
