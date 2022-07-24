4bbf3361e55a85007783f7092f6e272a9e295ac7
Antonova Mariia <mantonova@fil-it.ru>
2022-07-25 00:22:35 +0300

    divide commands call to each entity and common commands

--- homework-2/src/main/java/org/liga/service/impl/CommandHandlerService.java
+++ homework-2/src/main/java/org/liga/service/impl/CommandHandlerService.java
@@ -1,12 +1,16 @@
 package org.liga.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.liga.enums.Commands;
+import org.liga.enums.TaskCommands;
+import org.liga.enums.UserCommands;
+import org.liga.enums.CommonCommands;
 import org.liga.exception.WrongCommandParametersException;
 import org.liga.service.TaskService;
 import org.liga.service.UserService;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,15 +28,23 @@ public class CommandHandlerService {
         List<String> commandParameters = Arrays.asList(
                 inputCommand.trim().split(","));
 
-        String command = commandParameters.get(0).trim();
         try {
-            response = Commands.valueOf(command).action(userService, taskService, commandParameters);
+            String command = commandParameters.get(0).trim();
+            if(command.contains("USER")){
+                response = UserCommands.valueOf(command).action(userService, commandParameters);
+            }else if(command.contains("TASK")){
+                response = TaskCommands.valueOf(command).action(taskService, commandParameters);
+            }else{
+                response = CommonCommands.valueOf(command).action(commandParameters);
+            }
         } catch (WrongCommandParametersException e) {
-            response = e.getMessage() + "пожалуйста, попробуйте еще раз";
+            response = "Пожалуйста, попробуйте еще раз. " + e.getMessage();
         } catch (IllegalArgumentException e) {
-            response = e + "Команда не распознается, попробуйте еще раз";
+            response = "Команда не распознается, попробуйте еще раз. " + e.getMessage();
+        } catch (EntityNotFoundException | EmptyResultDataAccessException e){
+            response = "Такая сущность не существует. Подробнее: " + e;
         } catch (RuntimeException e){
-            response = e + "Упс, что-то сломалось...";
+            response = "Упс, что-то сломалось... " + e;
         }
         return response;
     }
