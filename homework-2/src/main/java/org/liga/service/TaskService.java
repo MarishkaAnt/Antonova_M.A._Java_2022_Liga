7396092a6cbf743aaded1c851a07a5186f97e151
Antonova Mariia <mantonova@fil-it.ru>
2022-07-25 00:24:51 +0300

    extend from AbstractService

--- homework-2/src/main/java/org/liga/service/TaskService.java
+++ homework-2/src/main/java/org/liga/service/TaskService.java
@@ -6,22 +6,28 @@ import org.liga.model.Task;
 import java.util.List;
 import java.util.Optional;
 
-public interface TaskService {
+public interface TaskService extends AbstractService<Task, Integer>{
 
     List<Task> findAllByStatus(Status status);
 
     void changeStatus(Integer id, Status status);
 
+    @Override
     List<Task> findAll();
 
+    @Override
     Optional<Task> findById(Integer id);
 
+    @Override
     Optional<Task> create(Task task);
 
+    @Override
     void deleteAll();
 
+    @Override
     void deleteById(Integer id);
 
+    @Override
     Optional<Task> update(Integer id, Task task);
 
 }
