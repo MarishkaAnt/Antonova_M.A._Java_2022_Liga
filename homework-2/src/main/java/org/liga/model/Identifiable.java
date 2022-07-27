9d36f7496c6b09a4f03ec3023674935e56029bb1
Antonova Mariia <mantonova@fil-it.ru>
2022-07-27 13:03:43 +0300

    add parent abstract class for uniformity of identification

---
+++ homework-2/src/main/java/org/liga/model/Identifiable.java
@@ -0,0 +1,25 @@
+package org.liga.model;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Абстрактный класс для единого задания идентификаторов сущностей
+ */
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+public abstract class Identifiable {
+    /** Идентификатор сущностей */
+    @Id
+    //@GeneratedValue(generator = "uuid")
+    //@GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+}
